325d6733f5fc93d397ea4514c7adb6ffe8244a48
dbswlals2 <bbdbswlals@gmail.com>
2023-03-17 17:28:49 +0900

    MenuManger

--- github/src/github/MenuManager.java
+++ github/src/github/MenuManager.java
@@ -1,8 +1,13 @@
 package github;
 
-public class Test {
+public class MenuManager {
 	public static void main(String[] args) {
-		System.out.println("hello");
-		System.out.println("git test");
+		
+		System.out.println("1. add Member");
+		System.out.println("2. delete Member");
+		System.out.println("3. edit Member");
+		System.out.println("4. view Member");
+		System.out.println("5. exit");
+		
 	}
 }
