06ee038ba9b1a3d696116202c48cb962dc9f30b0
dbswlals2 <bbdbswlals@gmail.com>
2023-03-23 15:10:48 +0900

    plus git test

--- github/src/github/MenuManager.java
+++ github/src/github/MenuManager.java
@@ -8,8 +8,7 @@ public class MenuManager {
 		System.out.println("3. edit Member");
 		System.out.println("4. view Member");
 		System.out.println("5. exit");
-		System.out.println("123123123");
-		System.out.println("123123123");
+		System.out.println("git test");
 		
 	}
 }
